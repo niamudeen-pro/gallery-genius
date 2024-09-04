8434a49f27d770d0e09bee9a0c1d0c1c3af5036c
niamudeen-pro <niamudeen6280@gmail.com>
2024-09-04 23:19:54 +0530

    basic setup

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+export default function LandingPage() {
+    return (
+        <>
+            <section className="section flex flex-col gap-4">
+                <h1>Gallery Genius</h1>
+                <div className="space-x-4">
+                    <button className="btn">File Upload</button>
+                    <button className="grayBtn">Download</button>
+                </div>
+            </section>
+        </>
+    );
+}
